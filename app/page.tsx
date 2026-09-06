bfe7444c0c63a3e6ec83467a7a24462fb63ec07f
angel.phoon <angel.phoon@bybit.com>
2026-09-06 18:35:55 +0800

    adjust button

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import ConnectWalletButton from "../connectWallet/ConnectWalletButton";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <main className="flex flex-col items-center gap-8">
-        <h1 className="text-4xl font-bold text-white">Online 2026</h1>
+    <div className="flex min-h-screen flex-col bg-black">
+      <header className="flex items-center justify-between p-6">
+        <h1 className="text-2xl font-bold text-white">Online 2026</h1>
         <ConnectWalletButton />
+      </header>
+      <main className="flex flex-1 items-center justify-center">
       </main>
     </div>
   );
